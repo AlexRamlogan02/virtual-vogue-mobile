6ffcf4d5598478130d52d63f8303a7cc4691be3d
Alexandra Ramlogan Salgado <aramlogan02@gmail.com>
2024-03-28 02:54:06 -0400

    Restructured layout, began setting up fragments for Login and Signup
    TODO: fix navbar

---
+++ app/src/main/java/com/alexandra/virtual_vogue_mobile/LoginViewModel.java
@@ -0,0 +1,7 @@
+package com.alexandra.virtual_vogue_mobile;
+
+import androidx.lifecycle.ViewModel;
+
+public class LoginViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/alexandra/virtual_vogue_mobile/landingPage.java
+++ app/src/main/java/com/alexandra/virtual_vogue_mobile/landingPage.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class signupActivity extends AppCompatActivity {
+public class landingPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_landing_page);
     }
 }
