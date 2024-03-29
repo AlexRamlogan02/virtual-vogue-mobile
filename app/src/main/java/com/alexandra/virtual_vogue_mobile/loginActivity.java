adafdfe84ff38779cb87f344ae88f371524a2d1c
jmauv <mauvaisjonathan@gmail.com>
2024-03-29 15:04:09 -0400

    fxfc

--- app/src/main/java/com/alexandra/virtual_vogue_mobile/loginActivity.java
+++ app/src/main/java/com/alexandra/virtual_vogue_mobile/loginActivity.java
@@ -3,49 +3,66 @@ package com.alexandra.virtual_vogue_mobile;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class loginActivity extends AppCompatActivity {
-
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     TextView text;
     OkHttpClient client;
     String url;
     EditText etLogin, etPassword;
     String login;
     String password;
+    JSONObject parameter;
+    Map<String, String> params;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        params = new HashMap<String, String>();
+
         client = new OkHttpClient();
         text = findViewById(R.id.titleText);
         url = "https://virtvogue-af76e325d3c9.herokuapp.com/api/Login";
-        etLogin = findViewById(R.id.usernameInput);
-        etPassword = findViewById(R.id.passwordInput);
-        login = etLogin.getText().toString();
-        password = etPassword.getText().toString();
+
+
+
 
         Button buttonLogin = findViewById(R.id.loginButton);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                etLogin = (EditText) findViewById(R.id.usernameInput);
+                etPassword = (EditText) findViewById(R.id.passwordInput);
+                login = etLogin.getText().toString();
+                password = etPassword.getText().toString();
+                params.put("login", login);
+                params.put("password", password);
+                parameter = new JSONObject(params);
                 post();
             }
 
@@ -53,31 +70,55 @@ public class loginActivity extends AppCompatActivity {
     }
 
     public void post(){
-        RequestBody requestBody = new FormBody.Builder()
-                .add("Login", login)
-                .add("Password", password)
-                .build();
+
+
+
+        RequestBody body = RequestBody.create(JSON, parameter.toString());
 
         Request request = new Request.Builder()
                 .url(url)
-                .post(requestBody)
+                .post(body)
+                .addHeader("content-type", "application/json; charset=utf-8")
                 .build();
 
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                text.setText("no");
                 e.printStackTrace();
             }
 
             @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) {
-                if (response.isSuccessful()) {
-                    text.setText("correct");
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                try {
+
+                    String user = "";
+                    String json = response.body().string();
+                    JSONObject jobj = new JSONObject(json);
+
+                    user = jobj.getString("userId");
+
+
+                    if (user.startsWith("-1")){
+                        loginActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                text.setText("wrong/password");
+                            }
+                        });
+                    }
+                    else{
+                        loginActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                startActivity(new Intent(loginActivity.this, MainActivity.class));
+                            }
+                        });
+                    }
+
+                } catch (JSONException e) {
+
                 }
-                else {
-                    text.setText("wrong");
-                }
+
             }
         });
 
--- app/src/main/java/com/alexandra/virtual_vogue_mobile/signupActivity.java
+++ app/src/main/java/com/alexandra/virtual_vogue_mobile/signupActivity.java
@@ -1,14 +1,48 @@
 package com.alexandra.virtual_vogue_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class signupActivity extends AppCompatActivity {
 
+    String url;
+    OkHttpClient client;
+    JSONObject parameter;
+    EditText fname, lname, email, password, login;
+
+    Map<String, String> params;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        client = new OkHttpClient();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        params = new HashMap<String, String>();
+        client = new OkHttpClient();
+        url = "https://virtvogue-af76e325d3c9.herokuapp.com/api/Register";
+
+        Button buttonRegister = findViewById(R.id.register);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
