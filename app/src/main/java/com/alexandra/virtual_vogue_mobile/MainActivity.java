e5e0ca4bd5d9c405b92726e8390a661d6e281c30
jmauv <mauvaisjonathan@gmail.com>
2024-03-29 01:42:19 -0400

    stuff

--- app/src/main/java/com/alexandra/virtual_vogue_mobile/MainActivity.java
+++ app/src/main/java/com/alexandra/virtual_vogue_mobile/MainActivity.java
@@ -1,15 +1,6 @@
 package com.alexandra.virtual_vogue_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoException;
-import com.mongodb.ServerApi;
-import com.mongodb.ServerApiVersion;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,29 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        String connectionString = "mongodb+srv://VirtualCloset:iaekLLKeJbmrHzts@cluster0.c5a9tds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-        ServerApi serverApi = ServerApi.builder()
-                .version(ServerApiVersion.V1)
-                .build();
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(new ConnectionString(connectionString))
-                .serverApi(serverApi)
-                .build();
-        // Create a new client and connect to the server
-        try (MongoClient mongoClient = MongoClients.create(settings)) {
-            try {
-                // Send a ping to confirm a successful connection
-                MongoDatabase database = mongoClient.getDatabase("VirtualCloset");
-                database.runCommand(new Document("ping", 1));
-                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-            } catch (MongoException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-        //If the user is logged in, open the main activity
-        //if not logged in, open log in activity
     }
 }
--- app/src/main/java/com/alexandra/virtual_vogue_mobile/loginActivity.java
+++ app/src/main/java/com/alexandra/virtual_vogue_mobile/loginActivity.java
@@ -1,14 +1,85 @@
 package com.alexandra.virtual_vogue_mobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class loginActivity extends AppCompatActivity {
+
+    TextView text;
+    OkHttpClient client;
+    String url;
+    EditText etLogin, etPassword;
+    String login;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        client = new OkHttpClient();
+        text = findViewById(R.id.titleText);
+        url = "https://virtvogue-af76e325d3c9.herokuapp.com/api/Login";
+        etLogin = findViewById(R.id.usernameInput);
+        etPassword = findViewById(R.id.passwordInput);
+        login = etLogin.getText().toString();
+        password = etPassword.getText().toString();
+
+        Button buttonLogin = findViewById(R.id.loginButton);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                post();
+            }
+
+        });
+    }
+
+    public void post(){
+        RequestBody requestBody = new FormBody.Builder()
+                .add("Login", login)
+                .add("Password", password)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                text.setText("no");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) {
+                if (response.isSuccessful()) {
+                    text.setText("correct");
+                }
+                else {
+                    text.setText("wrong");
+                }
+            }
+        });
+
     }
 }
