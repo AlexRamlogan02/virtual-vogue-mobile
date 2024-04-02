98f6b39cf048d1ef612ec9be27c0ed78e63c26a3
jmauv <mauvaisjonathan@gmail.com>
2024-04-02 15:11:21 -0400

    okkkkk

--- app/src/main/java/com/alexandra/virtual_vogue_mobile/MainActivity.java
+++ app/src/main/java/com/alexandra/virtual_vogue_mobile/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         BottomNavigationView navigationView = findViewById(R.id.nav_view);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().
                 findFragmentById(R.id.nav_host_fragment);
--- app/src/main/java/com/alexandra/virtual_vogue_mobile/loginFragment.java
+++ app/src/main/java/com/alexandra/virtual_vogue_mobile/loginFragment.java
@@ -3,7 +3,6 @@ package com.alexandra.virtual_vogue_mobile;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -13,21 +12,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
 
 public class loginFragment extends Fragment {
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    Map<String, String> params;
+    OkHttpClient client;
+    TextView text;
+    String url;
     String TAG = "loginFragment";
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: Called");
-
         View parentView =  inflater.inflate(R.layout.fragment_login, container, false);
 
         //place listeners on the edit boxes!
+        Button buttonLogin = getView().findViewById(R.id.loginButton);
+
 
         return parentView;
     }
-
     private void onButtonClick(View view){
         Log.d(TAG, "onButtonClick: Started");
 
@@ -35,3 +46,19 @@ public class loginFragment extends Fragment {
     }
 
 }
+
+/*
+params = new HashMap<String, String>();
+        client = new OkHttpClient();
+        text = getView().findViewById(R.id.loginText);
+        url = "https://virtvogue-af76e325d3c9.herokuapp.com/api/Login";
+        Button buttonLogin = getView().findViewById(R.id.loginButton);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+        text.setText("wrong/password");
+        }
+        });
+
+ */
