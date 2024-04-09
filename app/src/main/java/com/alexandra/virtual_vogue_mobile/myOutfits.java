5f90213a7068caabcf56d0fb77430b4819c78ed0
Alexandra Ramlogan Salgado <aramlogan02@gmail.com>
2024-04-09 19:10:36 -0400

    Open camera from fab
    Gonna change this

--- app/src/main/java/com/alexandra/virtual_vogue_mobile/myOutfits.java
+++ app/src/main/java/com/alexandra/virtual_vogue_mobile/myOutfits.java
@@ -9,17 +9,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.SearchView;
 
 import com.alexandra.virtual_vogue_mobile.databinding.FragmentMyOutfitsBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -27,6 +31,9 @@ import com.alexandra.virtual_vogue_mobile.databinding.FragmentMyOutfitsBinding;
  */
 public class myOutfits extends Fragment {
     String TAG = "myOutfits";
+    FloatingActionButton floatingActionButton;
+    //camera
+    Intent intent;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
@@ -35,6 +42,25 @@ public class myOutfits extends Fragment {
 
         View parentView =  inflater.inflate(R.layout.fragment_my_outfits, container, false);
         //stuff goes here!
+        floatingActionButton = parentView.findViewById(R.id.addToClosetButton);
+
+        floatingActionButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d(TAG, "onClick: Button Clicked");
+                        intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                        try {
+                            startActivity(intent);
+                        }
+                        catch(Exception e){
+                            Log.e(TAG, "onClick: Error opening camera", e);
+                        }
+                    }
+                }
+        );
+
 
         return parentView;
     }
