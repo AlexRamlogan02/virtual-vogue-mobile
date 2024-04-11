9472ee6193a380899c09abf468af759f6ee3614a
Alexandra Ramlogan Salgado <aramlogan02@gmail.com>
2024-04-11 00:19:14 -0400

    Open Camera working
    Need to implement adding clothes to DB

--- app/src/main/java/com/alexandra/virtual_vogue_mobile/AddImageActivity.java
+++ app/src/main/java/com/alexandra/virtual_vogue_mobile/AddImageActivity.java
@@ -2,10 +2,12 @@ package com.alexandra.virtual_vogue_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class AddImageActivity extends AppCompatActivity {
 
@@ -13,6 +15,7 @@ public class AddImageActivity extends AppCompatActivity {
     Button mOpenCameraButton;
     Button mSubmitButton;
     Button mCancelButton;
+    RadioGroup mRadioGroup;
 
     String TAG = "AddImageActivity";
 
@@ -26,16 +29,26 @@ public class AddImageActivity extends AppCompatActivity {
         mOpenCameraButton = findViewById(R.id.takePictureButton);
         mSubmitButton = findViewById(R.id.submitButton);
         mCancelButton = findViewById(R.id.cancelButton);
+        mRadioGroup = findViewById(R.id.clothingSelectionGroup);
 
-        Log.d(TAG, "onCreate: Initialized Buttons");
+        Log.d(TAG, "onCreate: Grabbed Buttons");
 
-        //set click listeners
+        //set click listeners for buttons
+
+        Log.d(TAG, "onCreate: Initialize Radio Group");
 
         mOpenCameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //open the camera
                 Log.d(TAG, "onClick: open camera");
+                Intent openCamera = new Intent("android.media.action.IMAGE_CAPTURE");
+                try {
+                    startActivity(openCamera);
+                }
+                catch(Exception e){
+                    Log.e(TAG, "onClick: Error Opening Camera", e);
+                }
             }
         });
         
