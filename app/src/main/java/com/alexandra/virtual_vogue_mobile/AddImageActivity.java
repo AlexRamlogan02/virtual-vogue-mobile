2a1a39457538038a8aaedb6e28d360c06371c66b
jmauv <mauvaisjonathan@gmail.com>
2024-04-16 18:42:23 -0400

    dw

--- app/src/main/java/com/alexandra/virtual_vogue_mobile/AddImageActivity.java
+++ app/src/main/java/com/alexandra/virtual_vogue_mobile/AddImageActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 
 public class AddImageActivity extends AppCompatActivity {
@@ -18,7 +21,9 @@ public class AddImageActivity extends AppCompatActivity {
     Button mCancelButton;
     RadioGroup mRadioGroup;
     SharedPreferences sharedPreferences;
-
+    ImageView imageView;
+    //String name = sharedPreferences.getString("user", null);
+    //String url = "https://virtvogue-af76e325d3c9.herokuapp.com/api/Upload/" + name;
     String TAG = "AddImageActivity";
 
     @Override
@@ -27,8 +32,7 @@ public class AddImageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_image);
 
 
-        String name = sharedPreferences.getString("user", null);
-        String url = "https://virtvogue-af76e325d3c9.herokuapp.com/api/Upload/" + name;
+        imageView = findViewById(R.id.imageView);
         mOpenLibraryButton = findViewById(R.id.OpenLibrary);
         mOpenCameraButton = findViewById(R.id.takePictureButton);
         mSubmitButton = findViewById(R.id.submitButton);
@@ -44,11 +48,14 @@ public class AddImageActivity extends AppCompatActivity {
         mOpenCameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 //open the camera
                 Log.d(TAG, "onClick: open camera");
-                Intent openCamera = new Intent("android.media.action.IMAGE_CAPTURE");
+                Intent openCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 try {
-                    startActivity(openCamera);
+                    startActivityForResult(openCamera, 1888);
+                    Bitmap photo = (Bitmap) openCamera.getExtras().get("data");
+                    imageView.setImageBitmap(photo);
                 }
                 catch(Exception e){
                     Log.e(TAG, "onClick: Error Opening Camera", e);
@@ -79,10 +86,6 @@ public class AddImageActivity extends AppCompatActivity {
                 Log.d(TAG, "onClick: Return");
             }
         });
-    }
-
-    public void uploadImage() {
-
     }
 
 }
