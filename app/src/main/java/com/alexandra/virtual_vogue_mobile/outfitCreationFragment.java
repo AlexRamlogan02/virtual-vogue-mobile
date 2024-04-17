45fd7cf958cfe4a6f3ae3b84e2b49adef5ed5cf7
jmauv <mauvaisjonathan@gmail.com>
2024-04-17 13:10:22 -0400

    Update outfitCreationFragment.java

--- app/src/main/java/com/alexandra/virtual_vogue_mobile/outfitCreationFragment.java
+++ app/src/main/java/com/alexandra/virtual_vogue_mobile/outfitCreationFragment.java
@@ -6,10 +6,16 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -23,6 +29,7 @@ public class outfitCreationFragment extends Fragment {
     SharedPreferences sharedPreferences;
     OkHttpClient client;
     String url, name;
+    String TAG = "createOutfits";
     TextView text;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -56,7 +63,25 @@ public class outfitCreationFragment extends Fragment {
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                String json = response.body().string();
+                JSONObject jobj = null;
+                JSONObject imageObj = null;
+                String imageURL;
+                String label;
 
+                try {
+                    jobj = new JSONObject(json);
+                    JSONArray jsonArray = jobj.getJSONArray("images");
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        imageObj = jsonArray.getJSONObject(i);
+                        JSONObject object = imageObj;
+
+                    }
+
+                } catch (JSONException e){
+                    throw new RuntimeException(e);
+                }
             }
         });
 
